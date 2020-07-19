5a16ad1d231c709cad4e3b8b77dede9429af90ef
Casey Price <Casey.Price@coxautoinc.com>
2020-07-19 16:47:24 -0400

    added seed repo code

---
+++ CustomerIntakeStarter/Controllers/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CustomerIntakeStarter.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class ExampleController : ControllerBase {
+        public ExampleController() {
+        }
+
+        [HttpGet]
+        public string Get() {
+            return "Hello World";
+        }
+    }
+}
